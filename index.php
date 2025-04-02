3d0ee7b06878b90315d03136515b21b75afe6393
LAhrendt <68784727+LAhrendt@users.noreply.github.com>
2025-04-02 14:26:33 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="da">
+<head>
+<title>Postnumre i Danmark, Færøerne og Grønland</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+</head>
+<body>
+
+<div class="w3-container w3-center">
+  <h1>Søg efter postnumre i Danmark, Færøerne og Grønland</h1>
+
+  <p>Indtast et postnummer i feltet og tryk på søg-knappen.</p>
+
+  <form class="w3-container" method="get">
+    <p><input class="w3-input w3-border w3-round-xxlarge" type="text" id="query" name="q"></p>
+    <p><input class="w3-btn w3-green w3-round" type="submit" value="Søg!"></p>
+  </form>
+
+  <div class="w3-container w3-left w3-left-align" id="results">
+
+  </div>
+</div>
+
+</body>
+</html>
